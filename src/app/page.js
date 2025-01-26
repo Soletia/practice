efad3ff3526b379440a3f058a3faf0ff76b4dec0
saclorum <saclorum@gmail.com>
2025-01-26 17:37:06 +0500

    hooks

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,24 @@
+'use client'
+import { useState } from "react";
+
 export default function Home() {
+  const [value, valueChange] = useState(0);
+  
   return (
-  <h1 className="text-5xl italic font-bold">Hello world!</h1>
-  );
-}
+
+<main>
+
+     <center>
+     <p className="coun">Счетчик:</p>
+      <div className="counter">
+        {value}<br/>
+        </div>
+        <hr className="divider"/>
+        <button className="text-white font-bold py-2 px-4 rounded" onClick={() => valueChange(value + 1)}>Увеличить число на 1</button> <br/>
+        <button className="text-white font-bold py-2 px-4 rounded m-4" onClick={() => valueChange(value - 1)}>Уменьшить число на 1</button>
+
+      </center>
+    </main>
+        );
+  };
+
