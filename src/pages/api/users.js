8edae23a99d6ef120de19e1f25f4b25f9a5b70f2
saclorum <saclorum@gmail.com>
2025-02-07 00:17:09 +0500

    registration

--- src/pages/api/users.js
+++ src/pages/api/users.js
@@ -1,34 +1,32 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import users from "@/app/data/user.json" 
- 
-export default function handler(req, res) {
+import users from "@/app/data/user.json";
+import { generatorSessionId } from "@/utility/gen";
+import { writeFileSync } from "fs";
 
-  
-
+export default async function handler(req, res) {
   if (req.method != "POST") {
-    res.status(500).json({message:`${req.method} is not allowed`})
+    res.status(500).json({ message: `${req.method} is not allowed` });
   }
-  let body = JSON.parse(req.body)
-  let usersList = users.users
+  let { email, password } = JSON.parse(req.body);
+
+  let usersList = users.users;
 
   //identification
-  let iden_user = usersList.filter((user)=> user.email == body.email)
-
-  
+  let iden_user = usersList.filter((user) => user.email == email);
 
   // authorization
   if (iden_user[0]) {
-    if (iden_user[0].password == body.password) {
-      // let SessionID = generateSessionId()
-      let SessionID = "TempId"
-      res.status(200).send({"Sessionid": SessionID})
+    if (iden_user[0].password == password) {
+      let SessionID = await generatorSessionId(11);
+
+      users.users[iden_user[0].id].SessionID = SessionID;
+
+      writeFileSync("src/app/data/user.json", JSON.stringify(users, null, 2));
+
+      res.status(200).send({ Sessionid: SessionID });
+    } else {
+      res.status(500).json({ message: "Неверные данные" });
     }
-    else{
-      res.status(500).json({message: "Неверные данные"})
-    }
+  } else {
+    res.status(500).json({ message: "Аккаунта с таким email не существует" });
   }
-  else{
-    res.status(500).json({message: "Аккаунта с таким email не существует"})
-  }
-  
 }
